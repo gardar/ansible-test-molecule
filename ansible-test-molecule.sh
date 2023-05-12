50d78028f3fb520f857e2ca4e150326b7b63e308
gardar <gardar@users.noreply.github.com>
2023-05-12 02:01:44 +0000

    refactor: tidy up
    Signed-off-by: gardar <gardar@users.noreply.github.com>

--- ansible-test-molecule.sh
+++ ansible-test-molecule.sh
@@ -8,14 +8,16 @@
 #
 # Version: 0.1.0
 
-
+# Set variables
 collection_root=$(pwd | grep -oP ".+\/ansible_collections\/\w+?\/\w+")
+role_root="$collection_root/roles/$role"
 target=${PWD##*/}
 role=$(expr "$target" : '\w*-\(\w*\)-\w*')
-role_root="$collection_root/roles/$role"
 scenario=$(expr "$target" : '\w*-\w*-\(\w*\)')
 ansible_version="$(ansible --version | head -1 | sed 's/[^0-9\.]*//g')"
 ansible_os_family="$(ansible localhost -m setup -a 'gather_subset=!all,!min,os_family filter=ansible_os_family' 2>/dev/null | grep -oP '(?<=ansible_os_family": ")[^"]+')"
+molecule_file=$collection_root/.config/molecule/config.yml
+yamllint_config_file=$collection_root/.yamllint.yml
 declare -A pkg=(
 	["debian"]="docker.io"
 	["redhat"]="docker"
@@ -44,53 +46,70 @@ check_version() {
     fi
 }
 
-check_version
+install_package_requirements() {
+	if [[ -v "pkg[${ansible_os_family,,}]" ]]; then
+		packages=${pkg[${ansible_os_family,,}]}
+		ansible localhost -m packages -a "name=$packages update_cache=true"
+	fi
+}
 
-# Install package requirements
-if [[ -v "pkg[${ansible_os_family,,}]" ]]; then
-	packages=${pkg[${ansible_os_family,,}]}
-	ansible localhost -m packages -a "name=$packages update_cache=true"
-fi
+install_pip_requirements() {
+	# Install test requirements from role
+	if [ -f "$role_root/test-requirements.txt" ]; then
+		python -m pip install -r "$role_root/test-requirements.txt"
+	fi
+	# Install test requirements from collection
+	if [ -f "$collection_root/test-requirements.txt" ]; then
+		python -m pip install -r "$collection_root/test-requirements.txt"
+	fi
+}
 
-# Install test requirements from role
-if [ -f "$role_root/test-requirements.txt" ]; then
-	python -m pip install -r "$role_root/test-requirements.txt"
-fi
-# Install test requirements from collection
-if [ -f "$collection_root/test-requirements.txt" ]; then
-	python -m pip install -r "$collection_root/test-requirements.txt"
-fi
+install_ansible_requirements() {
+	# Install ansible version specific requirements
+	if [ "$(printf '%s\n' "2.12" "$ansible_version" | sort -V | head -n1)" = "2.12" ]; then 
+		python -m pip install molecule molecule-plugins[docker]
+		ansible-galaxy collection install git+https://github.com/ansible-collections/community.docker.git
+		[ -f "$collection_root/requirements.yml" ] && ansible-galaxy collection install -r "$collection_root/requirements.yml"
+	elif [ "$(printf '%s\n' "2.10" "$ansible_version" | sort -V | head -n1)" = "2.10" ]; then
+		python -m pip install molecule molecule-docker
+		ansible-galaxy collection install git+https://github.com/ansible-collections/community.docker.git
+		[ -f "$collection_root/requirements.yml" ] && ansible-galaxy collection install -r "$collection_root/requirements.yml"
+	else
+		python -m pip install molecule molecule-docker
+		if [ -f "$collection_root/requirements.yml" ]; then
+			req_dir=$(mktemp -d)
+			requirements="$(awk '/name:/ {print $3}' <"$collection_root/requirements.yml") https://github.com/ansible-collections/community.docker.git"
+			for req in $requirements; do
+				git -C "$req_dir" clone --single-branch --depth 1 "$req"
+				req="${req##*/}"
+				req="${req%.*}"
+				ansible-galaxy collection build "$req_dir/$req" --output-path "$req_dir"
+				ansible-galaxy collection install "$req_dir/${req//./-}"-*.tar.gz
+			done
+		fi
+	fi
+}
 
-# Install ansible version specific requirements
-if [ "$(printf '%s\n' "2.12" "$ansible_version" | sort -V | head -n1)" = "2.12" ]; then 
-	python -m pip install molecule molecule-plugins[docker]
-	ansible-galaxy collection install git+https://github.com/ansible-collections/community.docker.git
-	ansible-galaxy collection install -r "$collection_root/requirements.yml"
-elif [ "$(printf '%s\n' "2.10" "$ansible_version" | sort -V | head -n1)" = "2.10" ]; then
-	python -m pip install molecule molecule-docker
-	ansible-galaxy collection install git+https://github.com/ansible-collections/community.docker.git
-	ansible-galaxy collection install -r "$collection_root/requirements.yml"
-else
-	python -m pip install molecule molecule-docker
-	req_dir=$(mktemp -d)
-	requirements="$(awk '/name:/ {print $3}' <"$collection_root/requirements.yml") https://github.com/ansible-collections/community.docker.git"
-	for req in $requirements; do
-		git -C "$req_dir" clone --single-branch --depth 1 "$req"
-		req="${req##*/}"
-		req="${req%.*}"
-		ansible-galaxy collection build "$req_dir/$req" --output-path "$req_dir"
-		ansible-galaxy collection install "$req_dir/${req//./-}"-*.tar.gz
-	done
-fi
+run_molecule() {
+	# Define config locations within collection
+	export MOLECULE_FILE=$molecule_file
+	export YAMLLINT_CONFIG_FILE=$yamllint_config_file
+	
+	# Unset ansible-test variables that break molecule
+	unset _ANSIBLE_COVERAGE_CONFIG
+	unset ANSIBLE_PYTHON_INTERPRETER
+	
+	# Run molecule test
+	cd "$role_root" || exit
+	molecule -c "$yamllint_config_file" test -s "$scenario"
+}
 
-# Define config locations within collection
-export MOLECULE_FILE=$collection_root/.config/molecule/config.yml
-export YAMLLINT_CONFIG_FILE=$collection_root/.yamllint.yml
+main() {
+	check_version
+	install_package_requirements
+	install_pip_requirements
+	install_ansible_requirements
+	run_molecule
+}
 
-# Unset ansible-test variables that break molecule
-unset _ANSIBLE_COVERAGE_CONFIG
-unset ANSIBLE_PYTHON_INTERPRETER
-
-# Run molecule test
-cd "$role_root" || exit
-molecule -c "$collection_root/.config/molecule/config.yml" test -s "$scenario"
+main
